7a86bc8650654b5eb0b69b8f69c376a2933c580d
Ankit Kapoor <ankitkapoor8437@gmail.com>
2022-02-11 02:20:57 +0530

    added header, added sign in page, added form input, addition of custom button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import './App.css'; 
-import { Switch ,Route } from 'react-router-dom';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
- 
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
-  return ( 
-    <Switch>
-      <Route exact  path='/' component={Homepage}/>
-      <Route path='/shop' component={ShopPage}/>
-    </Switch>
+  return (
+    <div>
+      <Header />
+
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+      </Switch>
+    </div>
+
   );
 }
 
