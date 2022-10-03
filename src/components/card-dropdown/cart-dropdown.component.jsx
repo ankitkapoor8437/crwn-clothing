d04bc28f663989787a9f2ea44d3e41fae18e2c89
Ankit Kapoor <ankitkapoor8437@gmail.com>
2022-10-03 06:57:05 +0530

    start development

--- src/components/card-dropdown/cart-dropdown.component.jsx
+++ src/components/card-dropdown/cart-dropdown.component.jsx
@@ -1,19 +1,29 @@
+import { useContext } from 'react';
+
 import Button from '../button/button.component';
 import './cart-dropdown.styles.scss';
 
+import { CartContext } from '../../contexts/cart.context';
 
-const CartDropdown = () =>
-{
-return(
-    <div className='cart-dropdown-container'>
-        <div className='cart-items'>
+import CartItem from '../cart-item/cart-itme.component';
+
+const CartDropdown = () => {
+
+const { cartItems } = useContext(CartContext);
+
+
+    return (
+        <div className='cart-dropdown-container'>
+            <div className='cart-items'>
+                
+                {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
+
+            </div>
             <Button buttonType='inverterd'>
                 GO TO CHECKOUT
             </Button>
         </div>
-    </div>
-)
-
-}
+    );
+};
 
 export default CartDropdown;
