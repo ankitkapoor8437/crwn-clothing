330d7bb489a3b433b6cdb02d8b29677dc16e03e3
Ankit Kapoor <ankitkapoor8437@gmail.com>
2022-11-07 13:40:32 +0530

    Implimented Nested routing anc made some changes in directory structure

---
+++ src/pages/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,31 @@
+import { useContext, Fragment } from 'react';
+
+import { CategoriesContext } from '../../contexts/categories.context';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+
+const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  return (
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => {
+        // <Fragment key={title}>
+                    //   <h2>{title}</h2>
+                    //   <div className="products-container">
+                    //     {categoriesMap[title].map((product) => (
+                    //       <ProductCard key={product.id} product={product} />
+                    //     ))}
+                    //   </div>
+                    // </Fragment>
+
+                    //changes after including the category preview thing
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </Fragment>
+  );
+};
+
+export default CategoriesPreview;
